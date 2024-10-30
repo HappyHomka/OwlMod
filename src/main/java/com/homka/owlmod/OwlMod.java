f87c888f6ce38653be46b20fe54597d0c563a891
Andrew <medalnik@gmail.com>
2024-10-30 14:57:11 +0400

    Initial commit

---
+++ src/main/java/com/homka/owlmod/OwlMod.java
@@ -0,0 +1,16 @@
+package com.homka.owlmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OwlMod implements ModInitializer {
+	public static final String MOD_ID = "owlmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/homka/owlmod/OwlModClient.java
@@ -0,0 +1,10 @@
+package com.homka.owlmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OwlModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
